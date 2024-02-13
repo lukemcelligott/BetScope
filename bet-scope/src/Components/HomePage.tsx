121124265fb2c36a9e0ebcb28a0f1a910df82151
lukemcelligott <lpm1006@sru.edu>
2024-02-13 17:38:49 -0500

    home page

--- bet-scope/src/Components/HomePage.tsx
+++ bet-scope/src/Components/HomePage.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 import './../App.css';
+import './Styles/HomePage.css'
 
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
 
 function HomePage() {
 
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      }));
+
     return (
         <div>
+            <h3>NBA</h3>
             <Card sx={{ minWidth: 275 }} className="card">
                 <CardContent>
                     <h3 className='font'>
-                        Portfolio
+                        Boston vs. Brooklyn
                     </h3>
-                    <p className='font p-height'>
-                        This portfolio site is built using <span className="skills">React</span> and <span className="skills">TypeScript</span>. I wanted to focus on improving my web design
-                        skills by using <span className="skills">CSS</span> and <span className="skills">MUI</span>. This project was a quick way of teaching myself about fundamental web principles as well as hosting.
-                    </p>
                     <div>
-                        
+                    <Grid container spacing={2}>
+                        <Grid item xs={4}>
+                            <Item>Spread<br></br>BOS -8.5</Item>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Item>ML<br></br>BOS -400</Item>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Item>O/U<br></br>102.5</Item>
+                        </Grid>
+                    </Grid>
                     </div>
                 </CardContent>
             </Card>
